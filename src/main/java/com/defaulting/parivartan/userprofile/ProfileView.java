34a5896f7b305653fbb2f68e6e9374c23c43ca10
RISHABH THUKRAL <rishabhthukral276@rediffmail.com>
2017-01-21 09:31:48 +0530

    Add Friend Front End Done.

--- src/main/java/com/defaulting/parivartan/userprofile/ProfileView.java
+++ src/main/java/com/defaulting/parivartan/userprofile/ProfileView.java
@@ -7,8 +7,12 @@ import com.vaadin.ui.AbsoluteLayout;
 import com.vaadin.ui.Alignment;
 import com.vaadin.ui.Button;
 import com.vaadin.ui.CssLayout;
+import com.vaadin.ui.FormLayout;
 import com.vaadin.ui.HorizontalLayout;
 import com.vaadin.ui.Notification;
+import com.vaadin.ui.PopupView;
+import com.vaadin.ui.TextField;
+import com.vaadin.ui.VerticalLayout;
 import com.vaadin.ui.Button.ClickEvent;
 import com.vaadin.ui.themes.ValoTheme;
 
@@ -20,14 +24,49 @@ public class ProfileView extends CssLayout implements View {
 		setSizeFull();
 		setStyleName("crud-view");
 		
+		FormLayout form = new FormLayout();
+		
+		TextField username = new TextField("Username");
+		Button addFriendPop = new Button("Add Friend", FontAwesome.USER_PLUS);
+		Button cancel = new Button("Cancel",FontAwesome.CLOSE);
+		addFriendPop.setStyleName(ValoTheme.BUTTON_FRIENDLY);
+		cancel.setStyleName(ValoTheme.BUTTON_DANGER);
+
+		HorizontalLayout buttonBar = new HorizontalLayout();
+		buttonBar.setSpacing(true);
+		buttonBar.setMargin(true);
+		buttonBar.addComponents(addFriendPop,cancel);
+		
+		username.setInputPrompt("Enter Friend's Username");
+		username.setWidth("6cm");
+		
+		form.addComponents(username,buttonBar);
+		
 		HorizontalLayout topBar = new HorizontalLayout();
+		
+		PopupView addFriends = new PopupView("Add friend", form);
+		
+		cancel.addClickListener(e -> {
+			addFriends.setPopupVisible(false);
+		});
+		addFriendPop.addClickListener(evt -> {
+			
+		});
 		
 		Button addFriend = new Button("right: 10px;top: 10px;");
 		addFriend.setCaption("Add Friend");
 		addFriend.setIcon(FontAwesome.PLUS_CIRCLE);
 		addFriend.addStyleName(ValoTheme.BUTTON_PRIMARY);
 		addFriend.addClickListener(evt -> {
-			//Side bar for friends
+			Notification.show("Ok So you want friends , Go watch F.R.I.E.N.D.S");
+//			if(form.isEnabled()){
+//				form.removeStyleName("visible");
+//	            form.setEnabled(false);
+//			}else{
+//				form.addStyleName("visible");
+//	            form.setEnabled(true);
+//			} DONT TOUCH THIS
+			addFriends.setPopupVisible(true);
 		});
 		
 		topBar.setSpacing(true);
@@ -36,7 +75,20 @@ public class ProfileView extends CssLayout implements View {
 		topBar.setComponentAlignment(addFriend, Alignment.TOP_RIGHT);
 		//topBar.setStyleName("top-bar");
 		
-		addComponent(topBar);
+		VerticalLayout barAndGridLayout = new VerticalLayout();
+        barAndGridLayout.addComponent(topBar);
+        //barAndGridLayout.addComponent(grid);
+        barAndGridLayout.setMargin(true);
+        barAndGridLayout.setSpacing(true);
+        barAndGridLayout.setSizeFull();
+        //barAndGridLayout.setExpandRatio(grid, 1);
+        barAndGridLayout.setStyleName("crud-main-layout");
+		
+		addComponent(barAndGridLayout);
+		//addComponent(form);
+		addComponent(addFriends);
+//		form.removeStyleName("visible");
+//        form.setEnabled(false);
 	}
 	
 	@Override
